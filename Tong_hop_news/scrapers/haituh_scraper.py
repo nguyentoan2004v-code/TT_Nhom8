a55cb948c58802381eb67eca9892a8b3bbe3bb91
nguyentoan2004v-code <nguyentoan2004v@gmail.com>
2025-11-12 19:47:55 +0700

    sua

---
+++ Tong_hop_news/scrapers/haituh_scraper.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_articles():
+    url = "https://www.24h.com.vn/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    articles = []
+    
+    # Tìm các thẻ tiêu đề của 24h
+    title_tags = soup.find_all(['h1','h2', 'h3'], class_='lh-100')
+    
+    for tag in title_tags[:5]:
+        link_tag = tag.find('a')
+        if link_tag:
+            title = link_tag.get('title') or link_tag.text.strip()
+            link = link_tag.get('href')
+            
+            if link and not link.startswith('http'):
+                link = 'https://www.24h.com.vn' + link
+            
+            if title and link:
+                articles.append({
+                    'title': title,
+                    'link': link,
+                    'content': get_article_content(link),
+                    'source': 4
+                })
+    
+    return articles
+
+def get_article_content(article_url):
+    try:
+        response = requests.get(article_url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        content_div = soup.find('div', class_='cms-body')
+        if content_div:
+            return content_div.text.strip()
+        else:
+            return "Không có nội dung"
+    except:
+        return "Lỗi khi lấy nội dung"
