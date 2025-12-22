c721b7355a8107fea316e553c256d51df00f4ad6
ChisPhong <chiphongle389@gmail.com>
2025-12-22 17:57:07 +0700

    'suarunauto.bat'

--- Tong_hop_news/run_auto.bat
+++ Tong_hop_news/run_auto.bat
@@ -1,19 +1,19 @@
 @echo off
-REM Enable UTF-8 encoding for console and Python
+REM 
 chcp 65001 >nul 2>&1
 set PYTHONUTF8=1
 set PYTHONIOENCODING=utf-8
 
-cd /d "D:\DOANCN\TT_Nhom8\Tong_hop_news"
+cd /d "%~dp0"
 
-REM Log start timestamp
+REM 
 echo [%date% %time%] Starting scraper >> scrape.log
 
-REM Run Python scraper unbuffered and tee output to console and scrape.log
-REM Uses PowerShell's Tee-Object so you can see live output in the opened CMD window
+REM 
+REM 
 powershell -NoProfile -Command "py -u main.py 2>&1 | Tee-Object -FilePath 'scrape.log' -Append"
 
-REM Log completion timestamp
+REM 
 echo [%date% %time%] Scraper finished >> scrape.log
 echo.
 
